9444a03c907ccb59b984e1716aee7ae7932a1310
Anton Teerioja <anton.s.teerioja@utu.fi>
2024-12-03 11:38:57 +0200

    Complete day3 Challenge 2

--- 3/day3.py
+++ 3/day3.py
@@ -1,21 +1,34 @@
 import re
 
+#Challenge 2
 with open('3/input.txt', 'r') as file:
   content = file.read()
 
-  correctList = re.findall("mul\([0-9]{,3},[0-9]{,3}\)", content)
+  correctList = re.findall("mul\([0-9]{,3},[0-9]{,3}\)|don't\(\)|do\(\)", content)
+  print(correctList)
 
   total = 0
+  isRunning = True
 
   for i in correctList:
-    start = i.index("(")
-    middle = i.index(",")
-    end = i.index(")")
 
-    total += int(i[start+1:middle]) * int(i[middle+1:end])
+    if re.match("don't\(\)", i):
+      isRunning = False
+      print("Stopped")
+
+    if re.match("do\(\)", i):
+      isRunning = True
+      print("Running")
+
+    elif isRunning == True:
+      print(i)
+      start = i.index("(")
+      middle = i.index(",")
+      end = i.index(")")
+
+      total += int(i[start+1:middle]) * int(i[middle+1:end])
+
     print(total)
-
-
 
   
 
