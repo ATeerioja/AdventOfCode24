9d898048c577a13c26674964f859de13b587b12f
Anton Teerioja <anton.s.teerioja@utu.fi>
2024-12-05 11:46:39 +0200

    Complete day5 Challenge 2

--- 5/day5.py
+++ 5/day5.py
@@ -7,8 +7,10 @@ with open('5/input.txt', 'r') as file:
   correctNumberList = []
   pageList = []
 
+  incorrect = []
   isTrue = True
-  for i in contentList:
+
+for i in contentList:
     if(i == ''):
       isTrue = False
       continue
@@ -18,12 +20,12 @@ with open('5/input.txt', 'r') as file:
     else:
       pageList.append(i)
 
-  for i in numberList:
-    correctNumberList.append(i.split(","))
+for i in numberList:
+  correctNumberList.append(i.split(","))
 
-  correct = []
+correct = []
 
-  for r in range(len(correctNumberList)):
+for r in range(len(correctNumberList)):
     isCorrect = True
 
     for x in range(len(correctNumberList[r])):
@@ -38,20 +40,23 @@ with open('5/input.txt', 'r') as file:
             beforeCounter += 1
     
       if(beforeCounter < len(correctNumberList[r]) - x - 1):
-        print(f"{correctNumberList[r]} Incorrect")
         isCorrect = False
+        incorrect.append(correctNumberList[r])
+        break
 
       for y in indiciesAfter:
         for z in range(len(correctNumberList[r]) - x - 1):
           if(correctNumberList[z] == pageList[y][0]+pageList[y][1]):
-            print(f"{correctNumberList[r]} Incorrect")
             isCorrect = False
+            incorrect.append(correctNumberList[r])
+            break
 
     if(isCorrect):
       correct.append(r)
 
-  sum = 0
-  for x in correct:
+sum = 0
+for x in correct:
+
     input_list = correctNumberList[x]
 
     middle = float(len(input_list))/2
@@ -60,4 +65,50 @@ with open('5/input.txt', 'r') as file:
     else:
         sum += int((input_list[int(middle)], input_list[int(middle-1)]))
 
-  print(sum)
+for r in range(len(incorrect)):
+  print(f"{r} / {len(incorrect)}")
+  for x in range(len(incorrect[r])):
+
+    indiciesBefore = [i for i in range(len(pageList)) if pageList[i][0]+pageList[i][1] == incorrect[r][x]]
+    
+    for x in range(len(incorrect[r])):
+      indiciesAfter = [i for i in range(len(pageList)) if pageList[i][3]+pageList[i][4] == incorrect[r][x]]
+      for i in indiciesAfter:
+        try:
+          if (incorrect[r].index(pageList[i][0]+pageList[i][1]) > incorrect[r].index(pageList[i][3]+pageList[i][4])):
+            
+            first = incorrect[r].index(pageList[i][0]+pageList[i][1])
+            second = incorrect[r].index(pageList[i][3]+pageList[i][4])
+
+            temp = incorrect[r][second]
+            incorrect[r][second] = incorrect[r][first]
+            incorrect[r][first] = temp
+
+        finally:
+          continue
+         
+    beforeCounter = 0
+    for y in indiciesBefore:
+      for z in incorrect[r]:
+        if(z == pageList[y][3]+pageList[y][4]):
+         beforeCounter += 1
+    
+    if(beforeCounter < len(incorrect[r]) - x - 1):
+      print("False")   
+    
+
+summa = 0
+for x in range(len(incorrect)):
+
+    input_list = incorrect[x]
+
+    middle = float(len(input_list))/2
+    if middle % 2 != 0:
+        summa += int(input_list[int(middle - .5)])
+    else:
+        summa += int((input_list[int(middle)], input_list[int(middle-1)]))
+
+
+
+      
+
