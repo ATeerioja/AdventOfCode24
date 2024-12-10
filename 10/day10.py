385468346717a6fcb264ce41cfd8115bc15bd2fd
ATeerioja <teerioja.opiskelu@gmail.com>
2024-12-10 19:24:50 +0200

    Ei toimi

--- 10/day10.py
+++ 10/day10.py
@@ -8,42 +8,67 @@ with open("10/input.txt", "r") as file:
     trailMapMatrix.append(list(i))
   
 startingLocations = []
+loops = 0
 
 for i in range(len(trailMapMatrix)):
   for x in range(len(trailMapMatrix[i])):
     if trailMapMatrix[i][x] == "0":
       startingLocations.append((i, x))
+
+def checkBranches(data):
+  start = data[0]
+  step = data[1]
+
+  branches = []
+  newStep = step
+
+  #Check if in matrix
+  if not(start[0] - 1 < 0 or start[1] + 1 >= len(trailMapMatrix[0])):      
     
-for i in startingLocations:
-  start = i
-  step = 0
+    if trailMapMatrix[start[0] - 1][start[1]] == str(step + 1):
+      branches.append((start[0] - 1,start[1]))
+      newStep = step + 1
 
-  while True:
-    print(start)
-    print(step)
+    if trailMapMatrix[start[0]][start[1] + 1] == str(step + 1):
+      branches.append((start[0],start[1] + 1))
+      newStep = step + 1
 
-    try:
-      if(start[0] - 1 < 0 or start[1] - 1 < 0):
-        if trailMapMatrix[start[0] - 1][start[1]] == str(step + 1):
-          start = ((start[0] - 1),(start[1]))
-          step += 1
+  if trailMapMatrix[start[0]][start[1] - 1] == str(step + 1):
+    branches.append((start[0],start[1] - 1))
+    newStep = step + 1
 
-        if trailMapMatrix[start[0]][start[1] - 1] == str(step + 1):
-          start = ((start[0]),(start[1] - 1))
-          step += 1
+  if trailMapMatrix[start[0] + 1][start[1]] == str(step + 1):
+    branches.append((start[0] + 1,start[1]))
+    newStep = step + 1
 
-      if trailMapMatrix[start[0]][start[1] + 1] == str(step + 1):
-        start = ((start[0]),(start[1] + 1))
-        step += 1
-      
-      if trailMapMatrix[start[0] + 1][start[1]] == str(step + 1):
-        start = ((start[0] + 1),(start[1]))
-        step += 1
+  
+  return ((branches, newStep))
 
-      if(trailMapMatrix[start[0]][start[1]]) == "9":
-        print("Found a Trail")
-        break
 
-    except IndexError:
-      print("Index out of bounds")
+def loop(start):
+  global loops
+  allBranches = checkBranches(start)
+  print(allBranches)
+
+  for branch in allBranches[0]:
+    next = ((branch, allBranches[1]))
+
+    if(allBranches[1] == 9):
+      print("Löytyi 9")
+      loops += 1
       break
+
+    print(next)
+    loop(next)
+    
+ 
+sum = 0
+for i in startingLocations:
+  print(i)
+  start = ((i, 0))
+  loop(start)
+  print(loops)
+  sum += loops
+  
+
+
